2ac17a22910e1907f4f8d0feeffac6908f07497f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-19 23:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2);
 }
 
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
