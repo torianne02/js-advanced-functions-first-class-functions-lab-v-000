ca1a7a940fa9ffda2dab65c4520b65db51d2ff3b
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-19 23:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
 }
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(drivers.length - 2, 2);
+}
